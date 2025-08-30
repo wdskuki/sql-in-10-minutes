9d8edfeb8b3f550ab3f3bd18395352515c7323f0
Dongsheng Wei <wei_ds@126.com>
2025-08-30 13:47:25 +0800

    10_7.sql

---
+++ 10_7.sql
@@ -0,0 +1,22 @@
+SELECT order_num, COUNT(*) AS order_lines 
+FROM OrderItems 
+GROUP BY order_num 
+ORDER BY order_lines;
+
+SELECT vender_name, MIN(prod_price) AS cheapest_item 
+FROM Products 
+GROUP BY vender_name 
+ORDER BY cheapest_item ASC;
+
+SELECT order_num, SUM(quantity) AS total_quantity 
+FROM OrderItems 
+GROUP By order_num
+HAVING SUM(quantity) >= 100 
+ORDER By order_num, total_quantity;
+
+SELECT order_num, SUM(item_price * quantity) AS total_price 
+FROM OrderItems
+GROUP BY order_num 
+HAVING total_price >= 1000 
+ORDER By order_num, total_price;
+
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # sql-in-10-minutes
+SQL必知必会（第五版）
